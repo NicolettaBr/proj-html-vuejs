ff4f67e50758199f676a7448264bac7f8db0fd2b
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-05-10 13:14:50 +0200

    inizio html/css

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,10 @@
 
 var app = new Vue({
   el: '#root',
-  data: {},
+  data: {
+    logo_nav: 'img/takeout-logo-2x-400x85.png',
+    test: 'test'
+  },
   methods: {}
 });
 
--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-           
+            logo_nav: 'img/takeout-logo-2x-400x85.png',
+            test: 'test',
         },
 
         methods: {
