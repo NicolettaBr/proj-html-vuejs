3558384768bbd25a78f19bcc31d4ef0bf2ea7378
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-05-12 13:04:39 +0200

    inizio footer

--- dist/app.js
+++ dist/app.js
@@ -14,7 +14,12 @@ var app = new Vue({
       logo: 'img/takeout-logo-2x-400x85.png',
       alt: 'logo'
     },
-    menu_nav: ['Home', 'Order Online', 'About', 'News', 'Contact Us']
+    menu_nav: ['Home', 'Order Online', 'About', 'News', 'Contact Us'],
+    menu_footer_1: ['Order Online', 'Appetizers', 'Burgers', 'Pizza', 'Fries', 'Sides', 'Desserts', 'Beverages', 'Specials'],
+    menu_footer_2: ['Navigate', 'Home', 'Alternate Home', 'Menu', 'About', 'News', 'Contact us', 'Cart'],
+    menu_footer_3: ['Information', 'My Account', 'Term Of Service', 'Privacy Policy'],
+    menu_footer_4: ['Opening Hours', 'Mon - 10AM to 11PM', 'Tue - 10AM to 11PM', 'Wed - 10AM to 11PM', 'Thu - 10AM to 11PM', 'Fri - 10AM to 11PM', 'Sat - 10AM to 1AM', 'Sun - 10AM to 11PM'],
+    menu_footer_5: ['Voucher']
   },
   methods: {}
 });
