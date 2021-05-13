89e4804fe9b25bc96626d421e9173f1c18c6d50f
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-05-13 09:28:23 +0200

    oggetti-menu-footer/ allineata colonna voucer footer

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 var app = new Vue({
     el: '#root',
         data: {
+
             logo_nav: {
                 logo: 'img/takeout-logo-2x-400x85.png',
                 alt: 'logo'
@@ -9,11 +10,30 @@ var app = new Vue({
 
             menu_nav: ['Home', 'Order Online', 'About', 'News', 'Contact Us'],
             
-            menu_footer_1: ['Order Online', 'Appetizers', 'Burgers', 'Pizza', 'Fries', 'Sides', 'Desserts', 'Beverages', 'Specials'],
-            menu_footer_2: ['Navigate', 'Home', 'Alternate Home', 'Menu', 'About', 'News', 'Contact us', 'Cart'],
-            menu_footer_3: ['Information', 'My Account', 'Term Of Service', 'Privacy Policy'],
-            menu_footer_4: ['Opening Hours', 'Mon - 10AM to 11PM', 'Tue - 10AM to 11PM', 'Wed - 10AM to 11PM', 'Thu - 10AM to 11PM', 'Fri - 10AM to 11PM', 'Sat - 10AM to 1AM', 'Sun - 11AM to 10PM'],
-            menu_footer_5: ['Voucher'],
+            menu_footer_1:{
+                title: 'Order Online',
+                menu_options: ['Appetizers', 'Burgers', 'Pizza', 'Fries', 'Sides', 'Desserts', 'Beverages', 'Specials']
+            },
+
+            menu_footer_2:{
+                title: 'Navigate',
+                menu_options: ['Home', 'Alternate Home', 'Menu', 'About', 'News', 'Contact us', 'Cart']
+            },
+
+            menu_footer_3:{
+                title: 'Information', 
+                menu_options: ['My Account', 'Term Of Service', 'Privacy Policy']
+            },
+
+            menu_footer_4:{
+                title: 'Opening Hours', 
+                menu_options: [ 'Mon - 10AM to 11PM', 'Tue - 10AM to 11PM', 'Wed - 10AM to 11PM', 'Thur - 10AM to 11PM', 'Fri - 10AM to 11PM', 'Sat - 10AM to 1AM', 'Sun - 11AM to 10PM']
+            },
+
+            menu_footer_5:{
+                title: 'Voucher'
+            },
+             
 
             apple_store:{
                 apple: 'img/app-store-badge-200x67.png',
